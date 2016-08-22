8363d6e09de6ce379a47db9b32f9dece62f3cb0c
Jordan Barkley <barkleyj@purdue.edu>
2016-08-22 11:06:55 -0400

    - jordan's initial commit

--- SoberDrivingIOS/AppDelegate.swift
+++ SoberDrivingIOS/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         // jordan was here
+        // jordan was here...again
         print("test")
         return true
     }
